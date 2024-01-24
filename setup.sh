bb65a4265ce27d47e1d27bd6a429a5fa0d39b404
tortugato <tortugato@pm.me>
2024-01-24 20:39:36 +0200

    Added option to choose if desired firewall is LuLu or LittleSnitch

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Clear the terminal before use
+clear
+
 # Get the path for the script directory
 script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
-USER=$(whoami)
 
 # Get the width of the terminal window
 width=$(tput cols)
@@ -17,53 +19,84 @@ NC='\033[0m'
 
 
 # Start of Script
-echo -e "\n${BOLD}Welcome to the setup${NC} \n \nPlease make sure to store your VPN of choice as ${ITALIC}vpn.pkg/vpn.dmg${NC} and a firewall of you choice as ${ITALIC}firewall.dmg${NC} in the directory of this script. \n(directory: $script_dir/) \n \nHit ${GREEN}ENTER${NC} to continue or ${RED}ctrl-c${NC} if you have to add the files:"
+echo -e "${BOLD}WELCOME TO THE SETUP${NC} \n\nPlease make sure to store your VPN of choice as ${ITALIC}vpn.pkg/vpn.dmg${NC} and a firewall of you choice as ${ITALIC}firewall.dmg${NC} in the directory of this script. \n(directory: $script_dir/) \n\nHit ${GREEN}ENTER${NC} to continue or ${RED}ctrl-c${NC} if you have to add the files"
+read 
+clear
 
-# Wait for Enter
+echo -e "After the initial setup of your system according to the README/Video we will now configure the macOS system for privacy and security."
+echo -e "\nHit ${GREEN}ENTER${NC} to continue"
+read
+clear
+
+echo -e "First we will start by installing a Firewall and a VPN. \nAfter this we will turn on the network connection."
+echo -e "\nHit ${GREEN}ENTER${NC} to continue"
+read
+
+# -----------------------------------------------------------------------------------------
+
+# Display the menu to choose which firewall to install
+echo "\nWhich Firewall do you want to install? (enter the number): "
+options=("LittleSnitch" "LuLu")
+
+select firewall_choice in "${options[@]}"; do
+    case $firewall_choice in
+        "LittleSnitch")
+            firewall_name="Little Snitch"
+            firewall_install_note="\n1) Uncheck Apple Services and iCloud Services during the last step of installation \n2) Allow Notifications and the System Extension by LittleSnitch \n3) Click on DemoMode as we won't be able to active the LittleSnitch license now \n4) Turn on Alert Mode (not Silent Mode)"
+            break
+            ;;
+        "LuLu")
+            firewall_name="LuLu"
+            firewall_install_note="\n1) Uncheck Allow Apple Programs and Allow Already Installed Applications during the installation \n2) Allow Notifications and the System Extension by LuLu"
+            break
+            ;;
+        *)
+            echo "Invalid option, please choose 1 or 2."
+            ;;
+    esac
+done
+
+echo -e "\nPlease make sure to follow the instructions by the $firewall_name installer carefully! \n${BOLD}Make sure: $firewall_install_note ${NC}"
+printf "%${width}s\n" | tr ' ' '-' 
+
+# Start the installation
+echo -e "\nHit ${GREEN}ENTER${NC} to start the installation (you will be prompted for your password)"
+read
+
+# Mount the DMG
+sudo hdiutil attach firewall.dmg
+
+# Get the directory of the mounted volume
+if [[ "$firewall_name" == "Little Snitch" ]]; then
+    app_name=$(find "/Volumes" -maxdepth 1 -type d -name "${firewall_name}*" | grep -E "${firewall_name} [0-9.]+" | head -n 1)
+elif [[ "$firewall_name" == "LuLu" ]]; then
+    app_name=$(find "/Volumes" -maxdepth 1 -type d -name "${firewall_name}*" | grep -E "${firewall_name} v[0-9.]+" | head -n 1)
+fi
+
+# Copy the .app file to /Applications
+rsync -a "${app_name}/${firewall_name}.app" "/Applications/"
+sleep 5
+
+# Unmount the DMG
+sudo hdiutil detach "${app_name}/"
+sleep 3
+
+#Open the firewall
+open "/Applications/${firewall_name}.app"
+clear
+
+echo -e "${GREEN}The Firewall was successfully installed.${NC}"
+echo -e "\n${RED}Please follow the setup process for LittleSnitch before continuing.${NC}"
+echo -e "\n${BOLD}REMINDER: $firewall_install_note"
+echo -e "\nHit ${GREEN}ENTER${NC} to continue"
 read 
 
-#Linebreak
-printf "%${width}s\n" | tr ' ' '-' 
-
-echo -e "\nAfter the initial setup of your system according to the ReadMe/Video we will now configure the macOS system for privacy and security."
-
-echo -e "\nHit ${GREEN}ENTER${NC} to continue"
-read
-# First Installs
-
-echo -e "\nFirst we will start by installing a firewall and a vpn. \nAfter this we will turn on the network connection. \n${BOLD}Note: We will configure LittleSnitch to block all connections made by Apple in the default profile and allow only the update connections in the update profile. More on that later${NC}"
-echo -e "\nHit ${GREEN}ENTER${NC} to continue"
-read
-echo -e "\nPlease make sure to follow the instructions by the LittleSnitch installer carefully! \n${BOLD}Make sure: \n1) Uncheck Apple Services and iCloud Services during the last step of installation \n2) Allow Notifications and the System Extension by LittleSnitch \n3) Click on DemoMode as we won't be able to active LittleSnitch now \n4) Turn on Alert Mode (not Silent Mode)${NC}"
-printf "%${width}s\n" | tr ' ' '-' 
-
-
-echo -e "\nHit ${GREEN}ENTER${NC} to start the installation"
-read
-
-# Install of firewall
-sudo hdiutil attach firewall.dmg
-rsync -a /Volumes/Little\ Snitch*/Little\ Snitch.app /Applications
-sleep 5
-sudo hdiutil unmount /Volumes/Little\ Snitch*
-open /Applications/Little\ Snitch.app 
-
-echo -e "\n${GREEN}The Firewall was successfully installed.${NC}"
-printf "%${width}s\n" | tr ' ' '-' 
-echo -e "\n${RED}Please follow the setup process for LittleSnitch before continuing.${NC}"
-echo -e "\n${BOLD}REMINDER: \n1) Uncheck Apple Services and iCloud Services during the last step of installation \n2) Allow Notifications and the System Extension by LittleSnitch \n3) Click on DemoMode as we won't be able to active LittleSnitch now \n4) Turn on Alert Mode (not Silent Mode)${NC}"
-
+# -----------------------------------------------------------------------------------------
 
 # Install of VPN
-echo -e "Now we will install the VPN."
-echo -e "\nHit ${GREEN}ENTER${NC} to continue"
-
-read
-sudo installer -package vpn.pkg -target /
 
 
-# Turn on network connection
-open /Applications/Mullvad\ VPN.app
+# -----------------------------------------------------------------------------------------
 
 
 # Brew
