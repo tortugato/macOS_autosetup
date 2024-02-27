e10094facb2aa0201f39bc5c106a318e74f18ff2
tortugato <tortugato@pm.me>
2024-02-27 19:59:48 +0100

    Update introduction.sh

--- scripts/introduction.sh
+++ scripts/introduction.sh
@@ -10,18 +10,19 @@ NC='\033[0m'
 function introduction(){
     echo -e "${BOLD}\nWELCOME TO THE SETUP${NC}"
     echo -e "This setup script aims to set up MacOS in the most private and secure way possible. \nThis script performs steps that I consider necessary and unavoidable to use MacOS privately and securely. \nI will continue to improve this script, so please keep an eye on the Github repository for possible changes. \nIf you have any suggestions for improvement, please post an issue to Github, I will try to take care of it as soon as possible.
+    \n\nIf you wish to install a firewall, a vpn, custom homebrew packages and/or your dotfiles please make sure to put them into the correct directories.
     \n\nLets get started. Here is an overview of the steps this script will guide you through.
-    \n1) Activating FileVault if not already activated
+    \n1) Activating FileVault
     \n2) Installation of helpful scripts
     \n3) Setting important settings
     \n4) Removing unneccessary features
-    \n5) Installation of a Firewall
-    \n6) Installation of a VPN
-    \n7) Installation of the Brew packagemanager
+    \n5) Installation of a Firewall (optional)
+    \n6) Installation of a VPN (optional)
+    \n7) Installation of the Brew Package Manager
     \n8) Installation of some basic packages
-    \n9) Installation of custom packages
-    \n10) Installation of dotfies
-    \n11) Cleaning up the look of MacOS"
+    \n9) Installation of custom packages (optional)
+    \n10) Installation of dotfies (optional)
+    \n11) Cleaning up the look of MacOS (optional)"
 
     echo -e "\n\nHit ${GREEN}ENTER${NC} to continue or ${RED}ctrl-c${NC} to abort."
     read
